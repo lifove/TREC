80715470b8b6a840ab42f292ca278e516a8c9597
JC <jc@lifove.net>
2015-11-04 16:17:28 -0500

    test driver

---
+++ TREC/test/ca/uwaterloo/tool/trec/test/MainTest.java
@@ -0,0 +1,17 @@
+package ca.uwaterloo.tool.trec.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import ca.uwaterloo.tool.trec.Main;
+
+public class MainTest {
+
+	@Test
+	public void test() {
+		String[] args={"-file", "data/Relink/Apache.arff", "-labelinfo", "buggy","-source", "data"};
+		Main.main(args);
+	}
+
+}
