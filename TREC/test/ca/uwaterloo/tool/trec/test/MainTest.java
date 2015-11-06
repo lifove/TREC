322ca4445561755eac38e4950e7cb150b3f34876
JC <jc@lifove.net>
2015-11-06 18:17:37 -0500

    Update Test Driver

--- TREC/test/ca/uwaterloo/tool/trec/test/MainTest.java
+++ TREC/test/ca/uwaterloo/tool/trec/test/MainTest.java
@@ -12,6 +12,26 @@ public class MainTest {
 	public void test() {
 		String[] args={"-file", "data/Relink/Apache.arff", "-labelinfo", "buggy","-source", "data","-experimental"};
 		Main.main(args);
+		args[1] = "data/Relink/Safe.arff";
+		Main.main(args);
+		
+		args[1] = "data/Relink/Zxing.arff";
+		Main.main(args);
+		
+		args[3] = "TRUE";
+		args[1] = "data/AEEEM/EQ.arff";
+		Main.main(args);
+		
+		args[1] = "data/AEEEM/JDT.arff";
+		Main.main(args);
+		
+		args[1] = "data/AEEEM/LC.arff";
+		Main.main(args);
+		
+		args[1] = "data/AEEEM/ML.arff";
+		Main.main(args);
+		
+		args[1] = "data/AEEEM/PDE.arff";
+		Main.main(args);
 	}
-
 }
